b299ff48d0ecdbc4b7accae1df21c0584a130e0c
Muhammad Awan Ardy Firmansyah <94277917+AwanArdy@users.noreply.github.com>
2023-10-28 14:09:52 +0800

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const Hapi = require('@hapi/hapi')
+const routes = require('./routes')
+
+const port = 9000
+const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost'
+
+const init = async ({ host, port }) => {
+	const server = Hapi.server({
+		host: host,
+		port: port,
+		routes: {
+			cors: {
+				origin: ['*']
+			}
+		}
+	})
+
+	server.route(routes)
+	server.start()
+	console.log(`Server Started at ${server.info.uri}`)
+}
+
+init({
+	host,
+	port,
+})
